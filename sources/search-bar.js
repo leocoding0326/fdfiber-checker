8a904ec9f5657c469051fe05585ca32b770db1bb
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-05-30 22:13:53 -0500

    New function added to search bar module
    wrote new function to filter database for a match with userInput
    then it will return the matching object

---
+++ sources/search-bar.js
@@ -0,0 +1,18 @@
+import addresses from "../database/database.js";//Imports database
+
+/* Algorithm for search bar:
+1. Receives a string from the user input;
+2. Makes the string all lowercase
+3. makes the string from database lowercase as well
+4. filters the database until a match is fund
+5. if not fund return an empty string
+6. if fund return the strings
+7. Filters the database to find cabinet & mst for the address;
+8. Push the info to the main js to show in the DOM.
+*/
+
+const userInputHandler = (userInput) => {
+    return addresses.filter((currentAddress) => {
+        userInput.toLowerCase() === currentAddress.address.toLowerCase();
+    });
+}
