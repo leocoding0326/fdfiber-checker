76b7e6641bbb162c2f1880de8375dbab6884100c
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-02 22:31:04 -0500

    Function show error created to manipulate error message

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/*
+Software Algorithm:
+1. User Types an address in input,
+2. User clicks search button,
+    2-1. If input is empty thrhow error;
+    2-2. If address not valid throw error;
+3. Input Value gets stored on a variable;
+4. Variable gets compared vs database address;
+    4-1. If not address fund throw an error asking to try a different address or ask admin to add it.
+5. If a result is fund render results in UI;
+*/
+
+
 import {autoComplete, userInputHandler} from './sources/search-bar.js'
 import addresses from '../database/database.js';
 
@@ -5,7 +18,8 @@ import addresses from '../database/database.js';
 
 const addressInput = document.getElementById('address-input');//Search Bar
 const suggestions = document.getElementById('suggestions')//ul under search bar
-const clearBtn = document.querySelector('.clear-btn')
+const clearBtn = document.querySelector('.clear-btn')//clear btn element
+const errorMsg = document.querySelector('.error-msg') //error message element
 
 
 const autoCompleteElement = () => {
@@ -32,12 +46,16 @@ const autoCompleteElement = () => {
     })
 }
 
-//Hide element on click
+//Hide element
 const hideElement = (element) => {
-    element.addEventListener('click', () => {
-        element.classList.add('hidden')
-    });
+     element.classList.add('hidden')
 };
+
+//Show element
+const showElement = (element) => {
+     element.classList.remove('hidden')
+};
+
 
 //toggle to remove clear button when input is empty
 
@@ -50,17 +68,10 @@ const clearBtnElement = () =>{
     })
 }
 
-/*
-Software Algorithm:
-1. User Types an address in input,
-2. User clicks search button,
-    2-1. If input is empty thrhow error;
-    2-2. If address not valid throw error;
-3. Input Value gets stored on a variable;
-4. Variable gets compared vs database address;
-    4-1. If not address fund throw an error asking to try a different address or ask admin to add it.
-5. If a result is fund render results in UI;
-*/
+const showError = (message) => {
+    errorMsg.textContent = message;
+    showElement(errorMsg)
+}
 
 autoCompleteElement()
 clearBtnElement()
