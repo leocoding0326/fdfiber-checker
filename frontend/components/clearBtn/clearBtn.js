5cf43455840774024ce50ee5903c0a9b707bde67
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-15 21:44:59 -0500

    New properties added as well as methods in clearBtn:
    propertie input created to receive user input;
    propertie element created to receive element to apply methods;
    method show and hide created to show and hide element when called;
    show input method created to show and hide element on input.

--- frontend/components/clearBtn/clearBtn.js
+++ frontend/components/clearBtn/clearBtn.js
@@ -1,5 +1,19 @@
 class clearBtn {
-    constructor() {
-        
+    constructor(input, element) {
+        this.input = input;
+        this.hidden = true;
+        this.element = element;
+    };
+    show() {
+        this.element.classList.remove('hidden');
     }
-}
+    hide() {
+        this.element.classList.add('hidden');
+    }
+    //show element on input
+    showOnInput() {
+        this.input.addEventListener('input', () => {
+            this.element.classList.toggle('hidden', this.input.value.length === 0);
+        });
+    };
+};
