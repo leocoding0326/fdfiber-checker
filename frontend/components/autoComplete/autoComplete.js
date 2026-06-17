c6d5cea8b913be61c53c6bb23704c7281931b88d
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-16 21:14:02 -0500

    New method runAutoComplete created: it will take the user input, iterate through the database ad find strings that includes the user input, return an array of matches.

--- frontend/components/autoComplete/autoComplete.js
+++ frontend/components/autoComplete/autoComplete.js
@@ -3,5 +3,17 @@ class AutoComplete {
         this.input = input,
         this.database = database,
         this.display = display
+        this.isOpen = false
+    };
+    runAutoComplete() {
+        const normalized = this.input.toLowerCase();
+        return this.database
+        .filter((match) => {
+            typeof match.database === 'string' && 
+            match.databse.toLowerCase().inlcudes(normalized);
+        })
+        .map(match => match.database)
+        .slice(0, 5);
     }
-}
+
+};
