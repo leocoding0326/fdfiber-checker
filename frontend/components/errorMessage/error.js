4af9147722470f1a0537f44798ca33425bbdefd5
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-18 23:16:50 -0500

    Completed error class

--- frontend/components/errorMessage/error.js
+++ frontend/components/errorMessage/error.js
@@ -4,26 +4,41 @@ class ErrorMessage {
         message,
         input,
         database,
-        displayResult,
+        key
     }) {
         this.displayError = displayError;
         this.message = message;
         this.input = input;
         this.database = database;
-        this.displayResult = displayResult;
-        this.isValid = true
-    }
-    addressExist() {
-        return database.some(profile => profile.address.toLowerCase() === input.toLowerCase());
-    }
+        this.isValid = true;
+        this.key = key
+    };
+
+    inputExist() {
+        return this.database.some(profile => profile[this.key]?.toLowerCase() === this.input.value.toLowerCase());
+    };
     
     validation() {
-        if (this.addressExist) {
-            this.isValid 
+        const exist = this.inputExist();
+        const value = this.input.value.trim()
+        if (!exist || value.length === 0) {
+            this.isValid = false; 
         }
-        else (!this.isValid);
-    }
+        else this.isValid = true;
+    };
 
-    
+    executeError() {
+        if(!this.isValid) {
+            this.displayError.hidden = false
+            this.displayError.innerHTML = this.message;
+        } else {
+            this.displayError.innerHTML = '';
+            this.displayError.hidden = true;
+        }
+    };
 
-}
+    hideError() {
+        this.displayError.hidden = true;
+        this.displayError.innerHTML = '';
+    };
+};
