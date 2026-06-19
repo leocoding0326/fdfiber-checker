adc6a293c9539a453906c76bfe049c2549bfe829
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-19 09:01:55 -0500

    Deleted all responsabilities from error besides show and hide

--- frontend/components/errorMessage/error.js
+++ frontend/components/errorMessage/error.js
@@ -1,44 +1,15 @@
 export class ErrorMessage {
     constructor({
         displayError,
-        message,
-        input,
-        database,
-        key
+        message
     }) {
         this.displayError = displayError;
         this.message = message;
-        this.input = input;
-        this.database = database;
-        this.isValid = false;
-        this.key = key
-    };
-
-    init() {
-        this.executeError();
-    }
-
-    inputExist() {
-        return this.database.some(profile => profile[this.key]?.toLowerCase() === this.input.value.toLowerCase());
     };
     
-    validation() {
-        const exist = this.inputExist();
-        const value = this.input.value.trim()
-        if (!exist || value.length === 0) {
-            this.isValid = false; 
-        }
-        else this.isValid = true;
-    };
-
     executeError() {
-        if(!this.isValid) {
-            this.displayError.hidden = false
-            this.displayError.innerHTML = this.message;
-        } else {
-            this.displayError.innerHTML = '';
-            this.displayError.hidden = true;
-        }
+        this.displayError.hidden = false;
+        this.displayError.innerHTML = this.message;
     };
 
     hideError() {
