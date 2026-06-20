39d1f03f863cb1c6f4e38891c406a3559de7db8c
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-06-20 15:31:16 -0500

    showElement and hideElement functions created:
    will add or remove html hidden propertie on element parameter on call

---
+++ frontend/utilities/showHide.js
@@ -0,0 +1,5 @@
+// Functions can be called to show or hide an element, using html hidden propertie
+
+const showElement = element => element.hidden = false;
+
+const hideElement = element => element.hidden = true;
